3c8d71bfb335d554361ae6b3984cdcdf7c0ec017
moinul75 <102654562+moinul75@users.noreply.github.com>
2022-04-04 13:35:12 +0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+# celcius to fahrenheit
+
+#fahrenheit calculate
+
+celcius = 37.5
+
+fahrenheit = (celcius * 1.8 ) + 32
+
+print(f"celcius {celcius} degree is equal to fahrenheit {fahrenheit} degree")
+
+#fahrenheit to calcius
+
+fahrenheit = 500
+
+calcius = (fahrenheit - 32 )/ 1.8
+
+print(f"fahrenheit {fahrenheit}  degree is equal to calcius {calcius} degree")
